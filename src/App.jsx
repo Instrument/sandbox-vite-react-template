11470a2e5550a8f0981a09a8fbbbe221953a1070
Lorraine Li <45266509+lorrrr@users.noreply.github.com>
2025-01-06 12:29:32 -0500

    Added instructions for adding a Route

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import SnakeGame from "./views/SnakeGame";
-import Home from "./views/Home";
+import Home from "./Home";
 
 function App() {
   return (
